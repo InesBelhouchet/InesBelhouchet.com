eb0d7780c70d100eed719016f422b59bed94f17a
ines belhouchet <inesbelhouchet@iness-MacBook-Pro.local>
2021-01-09 16:28:28 +0100

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,25 @@
+package com.inesbelhouchetprojects
+
+import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
+import io.ktor.application.*
+import io.ktor.freemarker.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
+    routing {
+        static("/static") {
+            resources("files")
+        }
+    }
+}
